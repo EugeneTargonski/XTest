6c4dd40fffdb99fed1f9468211b015b758c4ee05
eugene_targonski <tevstim@gmail.com>
2020-01-10 15:05:23 +0300

    0.01

--- XTest/XTest/XTest/MainPage.xaml.cs
+++ XTest/XTest/XTest/MainPage.xaml.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Auth;
 using Xamarin.Forms;
+
+
 
 namespace XTest
 {
@@ -13,9 +16,56 @@ namespace XTest
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public static readonly string GOOGLE_ID = "21170702167-jriifs3n881an76deo58c5lb92l6fj11.apps.googleusercontent.com";
+        public static readonly string GOOGLE_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+        public static readonly string GOOGLE_AUTH = "https://accounts.google.com/o/oauth2/auth";
+        public static readonly string GOOGLE_REDIRECTURL = "https://www.googleapis.com/plus/v1/people/me";
+        public static readonly string GOOGLE_REQUESTURL = "https://www.googleapis.com/oauth2/v2/userinfo";
+        readonly OAuth2Authenticator authenticator = new OAuth2Authenticator(GOOGLE_ID, GOOGLE_SCOPE, new Uri(GOOGLE_AUTH), new Uri(GOOGLE_REDIRECTURL));
+
+        private readonly Label label = new Label
+        {
+            VerticalTextAlignment = TextAlignment.Center,
+            HorizontalTextAlignment = TextAlignment.Center,
+            Text = "Welcome to Xamarin Forms!",
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+        };
+
+
         public MainPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            StackLayout stackLayout = new StackLayout();
+            Button button = new Button
+            {
+                Text = "Нажми!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            stackLayout.Children.Add(label);
+            stackLayout.Children.Add(button);
+            this.Content = stackLayout;
+        }
+
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = "Нажато!";
+            //button.BackgroundColor = Color.Red;
+        }
+
+        private void OnAuthCompleted(object sender, System.EventArgs e)
+        {
+            //OAuth2Authenticator auth = (OAuth2Authenticator)sender;
+            if (authenticator.IsAuthenticated())
+            {
+                //Authentication failed Do something
+                label.Text = "Is Authenticated";
+                return;
+            }
+            label.Text = "Isn't Authenticated";
         }
     }
 }
